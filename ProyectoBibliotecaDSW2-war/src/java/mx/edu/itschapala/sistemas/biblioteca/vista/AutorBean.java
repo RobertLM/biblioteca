0cc2e7cf6d1083e15c01409864eb10725ac57752
Admin <Admin@computo3-08.localdomain>
2015-03-20 12:03:33 -0600

    autor y editar crear

--- ProyectoBibliotecaDSW2-war/src/java/mx/edu/itschapala/sistemas/biblioteca/vista/AutorBean.java
+++ ProyectoBibliotecaDSW2-war/src/java/mx/edu/itschapala/sistemas/biblioteca/vista/AutorBean.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 import mx.edu.itschapala.sistemas.biblioteca.bl.AutorBLLocal;
 import mx.edu.itschapala.sistemas.biblioteca.modelo.Autor;
 
@@ -19,23 +20,81 @@ import mx.edu.itschapala.sistemas.biblioteca.modelo.Autor;
 @ManagedBean
 @SessionScoped
 public class AutorBean {
-    //Llamadas a los EJB
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------   
+//                                                                  Llamadas a los EJB
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
     @EJB
     private AutorBLLocal autorBL;
-
-    //Caracteristicas
-    private List<Autor>lista;
-    
-    //Constructor obligatorio
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                   Caracteristicas
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    private List<Autor>lista;//Variable con la lista de autores
+    private Autor autor;//objeto no autoadministrado
+    private Accion accion;//Variable para saber que s va a realizar
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                     Constructor obligatorio
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
     public AutorBean() {
+        autor=new Autor();
     }
     
-    
-    //Metodos GET y SET necesarios
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------    
+//                                                                 Metodos GET y SET necesarios
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     public List<Autor> getLista(){
         lista=autorBL.getLista();
         return lista;
     }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
     
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                   Acciones
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    public String procesarPeticion(){
+        switch(accion){
+            case NUEVO:
+                 autorBL.registrar(autor);
+                 break;
+            case EDITAR:
+                 autorBL.modificar(autor);
+                 break;
+            case ELIMINAR:
+                 autorBL.eliminar(autor);
+                 break;
+        }
+        accion=Accion.NADA;
+        return "AutorLista";
+    }
+    
+    public String procesarCancelar(){
+        if(accion==Accion.ELIMINAR){
+            return "AutorLista";
+        }else{
+            return "AutorCrearEditar";
+        }
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                   Eventos
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------   
+    public void prepararNuevo(ActionEvent e){
+        autor=new Autor();
+        accion=Accion.NUEVO;
+    }
+    
+    public void prepararEditar(ActionEvent e){
+        accion=Accion.EDITAR;
+    }
+    public void prepararEliminar(ActionEvent e){
+        accion=Accion.ELIMINAR;
+    }
 }
